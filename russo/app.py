f4fcead14684e83c54bf8eb1c051da8844a18f95
russo -leonardo <russo.leonardo@iisgalvanimi.edu.it>
2025-04-04 11:02:12 +0000

    commit1

--- russo/app.py
+++ russo/app.py
@@ -1,7 +1,48 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/') #visitiamo (`/`), la funzione home() viene eseguita.
-def home():
-    return render_template("index.html")
-if __name__ == '__main__':
+from flask import Flask, render_template, request, redirect, url_for #imparare memoria
+
+lista = []
+
+app = Flask(__name__)#imparare a memoria, è il nome dell'app Flask che dò io.
+
+x = 'ciao'
+
+@app.route('/') #visitiamo l'url (`/`), la funzione home() viene eseguita.  #imparare a memoria
+def home():#definisco funzione home
+    return render_template("index.html", paperino = x, lista=lista) 
+
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+    #ottiene elemento dal form
+    elemento = request.form['elemento']
+    #aggiunge alla lista
+    if elemento:
+        lista.append(elemento)
+    return redirect(url_for('home'))
+
+   
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+    if 0 <= indice < len(lista):
+       lista.pop(indice)
+    return redirect(url_for('home'))
+
+@app.route('/elimina tutto/<int:indice>',methods=['POST'])
+def elimina_tutto():
+        lista.clear()
+        return redirect(url_for('home'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':#imparare a memoria
    app.run(debug=True)
